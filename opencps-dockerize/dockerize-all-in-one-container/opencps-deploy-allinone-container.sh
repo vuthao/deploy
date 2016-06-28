42ce8656bf7851520eba2a253b4f0dfdc45e844e
bachkhoabk47 <bachkhoabk47@gmail.com>
2016-06-28 14:20:50 +0700

    Update script file

--- opencps-dockerize/dockerize-all-in-one-container/opencps-deploy-allinone-container.sh
+++ opencps-dockerize/dockerize-all-in-one-container/opencps-deploy-allinone-container.sh
@@ -12,16 +12,34 @@ DIR_CURR=$DIR/docker-compose
 
 M_PLATFORM_UBUNTU=`python -mplatform |grep -Eo "Ubuntu"`
 M_PLATFORM_CENTOS=`python -mplatform |grep -Eo "centos"`
-M_PLATFORM_FEDORA=`python -mplatform |grep -Eo "fedora"`
+M_PLATFORM_FEDORA=`python3 -mplatform |grep -Eo "fedora"`
 echo $M_PLATFORM_CENTOS
+
+if [[ $EUID -ne 0 ]]; then
+ echo "You are not running with root permission, so that why should chage to root for executing to install something"
+ if [[ $M_PLATFORM_UBUNTU == "Ubuntu" ]]; then
+  echo "Type: sudo -s before running script"
+  sudo -s
+ elif [ $M_PLATFORM_CENTOS == "centos" ]; then
+  echo "Type: su - before running script"
+  exit;
+ elif [ $M_PLATFORM_FEDORA == "fedora" ]; then
+  echo "Type: su - before running script"
+  exit;
+ else
+  echo "This could not ecognize any distro of Linux, please check it again"
+ fi 
+else
+ echo "Good! You are running with root permission!"
+fi
 
 function app_update {
  if [[ $M_PLATFORM_UBUNTU == "Ubuntu" ]]; then
-  sudo apt-get update -y
+  apt-get update -y
  elif [ $M_PLATFORM_CENTOS == "centos" ]; then
-  sudo yum update -y
+  yum update -y
  elif [ $M_PLATFORM_FEDORA == "fedora" ]; then
-  sudo yum update -y
+  yum update -y
  else
   echo "This could not ecognize any distro of Linux, please check it again"
  fi
@@ -32,19 +50,19 @@ function install_svn {
   if which svn >/dev/null; then
     echo "svn installed, so next to install other app"
   else
-    sudo apt-get install subversion -y
+    apt-get install subversion -y
   fi
  elif [[ $M_PLATFORM_CENTOS == "centos" ]]; then
   if which svn >/dev/null; then
     echo "svn installed, so next to install other app"
   else
-    sudo yum install svn -y
+    yum install svn -y
   fi
  elif [[ $M_PLATFORM_FEDORA == "fedora" ]]; then
   if which svn >/dev/null; then
     echo "svn installed, so next to install other app"
   else
-    sudo yum install svn -y
+    yum install subversion -y
   fi
  else
   echo "This could not ecognize any distro of Linux, please check it again"
@@ -55,9 +73,9 @@ function chkconfig_docker {
  if [[ $M_PLATFORM_UBUNTU == "Ubuntu" ]]; then
   echo "Version Ubuntu > 14.10, this just needs"
  elif [[ $M_PLATFORM_CENTOS == "centos" ]]; then
-  sudo chkconfig docker on
+  chkconfig docker on
  elif [[ M_PLATFORM_FEDORA == "fedora" ]]; then
-  sudo chkconfig docker on
+  chkconfig docker on
  else 
   echo "This could not ecognize any distro of Linux, please check it again"
  fi
@@ -67,8 +85,8 @@ function install_docker {
  if which docker >/dev/null; then
     echo "docker installed, so go to next step"
  else
-    sudo curl -fsSL https://get.docker.com/ | sh
-    sudo service docker start
+    curl -fsSL https://get.docker.com/ | sh
+    service docker start
  fi
 
  if which docker-compose >/dev/null; then
@@ -76,9 +94,9 @@ function install_docker {
  else
     ############## Installing Docker-compose on Centos 7, Ubuntu, Fedora ###############
     # Download and run script for installing docker-compose
-    sudo wget https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` -O /usr/local/bin/docker-compose
+    wget https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` -O /usr/local/bin/docker-compose
     # Chown permission
-    sudo chmod +x /usr/local/bin/docker-compose
+    chmod 755 /usr/local/bin/docker-compose
  fi
 
 }
@@ -86,13 +104,20 @@ function install_docker {
 function check_distro {
  app_update
  
- sudo apt-get install curl -y
+ apt-get install curl -y
  install_svn
  install_docker
  chkconfig_docker
 
 }
 check_distro
+
+############## Installing Docker-compose on Centos 7, Ubuntu, Fedora ###############
+# Download and run script for installing docker-compose
+#sudo wget https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` -O /usr/local/bin/docker-compose
+# Chown permission
+#sudo chmod +x /usr/local/bin/docker-compose
+
 
 ##########################################################################
 
@@ -109,14 +134,15 @@ if [ -d "$DIR_CURR" ]; then
   echo "You enteredi: $text"
   
   if [ $text == "y" ]; then
-    sudo svn export https://github.com/VietOpenCPS/deploy.git/trunk/opencps-dockerize/dockerize-all-in-one-container/docker-compose --force
+    svn export https://github.com/VietOpenCPS/deploy.git/trunk/opencps-dockerize/dockerize-all-in-one-container/docker-compose --force
+    sh -c `cd $DIR_CURR && docker-compose -f docker-compose.yml up -d`
+    echo "Done! You can open browser with the address: localhost:8080 for testing application. Thanks"
   else
     echo "You've exited running script"
     exit;
   fi 
 else
-  sudo svn export https://github.com/VietOpenCPS/deploy/trunk/opencps-dockerize/dockerize-all-in-one-container/docker-compose
+  svn export https://github.com/VietOpenCPS/deploy/trunk/opencps-dockerize/dockerize-all-in-one-container/docker-compose
+  sh -c `cd $DIR_CURR && docker-compose -f docker-compose.yml up -d`
+  echo "Done! You can open browser with the address: localhost:8080 for testing application. Thanks"
 fi
-
-sh -c `cd $DIR_CURR && sudo docker-compose -f docker-compose.yml up -d`
-echo "Done! You can open browser with the address: localhost:8080 for testing application. Thanks"
